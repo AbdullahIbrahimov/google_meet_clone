11738d4d006682a24c5b7aef747a7eb775c85c45
YadullaIbrahimov <YadullaIbrahimov@users.noreply.github.com>
2021-09-26 19:24:29 -0400

    created react base data and html, css part1

---
+++ src/Components/UI/Header/Header.js
@@ -0,0 +1,15 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo">
+        <img src="https://www.gstatic.com/meet/google_meet_horizontal_wordmark_2020q4_1x_icon_124_40_2373e79660dabbf194273d27aa7ee1f5.png" />
+        <span className="help-text">Meet</span>
+      </div>
+      <div className="action-btn"></div>
+    </div>
+  );
+};
+
+export default Header;
